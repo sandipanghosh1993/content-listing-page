fde32e0afa3bb3d3e2ba860ecf3339253fa87282
Sandipan Ghosh <sandipan.ghosh01@sap.com>
2020-08-17 00:09:15 +0530

    Updates

---
+++ precache-manifest.f20047181ea9ff2e1b9b0ea7f7ee4714.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e9d650fb13f5d21d07824c400df120",
+    "url": "/content-listing-page/index.html"
+  },
+  {
+    "revision": "4f9fb3d0412bdb860c47",
+    "url": "/content-listing-page/static/js/2.ce0b5e59.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/content-listing-page/static/js/2.ce0b5e59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f6d914dd3179a8246d7",
+    "url": "/content-listing-page/static/js/3.ac3c12a3.chunk.js"
+  },
+  {
+    "revision": "7e5595f871b891b218ad",
+    "url": "/content-listing-page/static/js/main.274dbdc1.chunk.js"
+  },
+  {
+    "revision": "57c0c5eabe0d69c4807b",
+    "url": "/content-listing-page/static/js/runtime-main.f4a1154c.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/content-listing-page/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/content-listing-page/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/content-listing-page/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/content-listing-page/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/content-listing-page/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/content-listing-page/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/content-listing-page/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/content-listing-page/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/content-listing-page/static/media/poster9.65d6d500.jpg"
+  }
+]);
